ba23bb67ddf61ef1b19213b20b95496dd2219da6
Jared Windover <jaredwindover@gmail.com>
2015-06-18 19:12:44 -0400

    Initial app, some dependencies

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+var port = provess.env.PORT || 3000;
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+    res.json({message:'Initial message'});
+});
+
+app.use('/api', router);
+
+app.listen(port);
+
+console.log('App running on port ' + port);
